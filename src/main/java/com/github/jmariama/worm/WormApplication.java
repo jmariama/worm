3236309c075cf1ac6cc83eb36f363102afb69f84
mariama mercy <jmariamercy@gmail.com>
2026-09-11 13:41:14 -0400

    initialization

---
+++ src/main/java/com/github/jmariama/worm/WormApplication.java
@@ -0,0 +1,13 @@
+package com.github.jmariama.worm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WormApplication.class, args);
+	}
+
+}
