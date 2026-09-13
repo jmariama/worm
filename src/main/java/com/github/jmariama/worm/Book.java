a70f7607520152de984c41f79a8c9e6efb0f38da
mariama mercy <jmariamercy@gmail.com>
2026-09-12 22:37:15 -0400

    basic book logic

--- src/main/java/com/github/jmariama/worm/Book.java
+++ src/main/java/com/github/jmariama/worm/Book.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 
 @Entity
 public class Book {
-    private int id;
+    private Long id;
     private String title;
     private String author;
     private int status;
@@ -13,6 +13,77 @@ public class Book {
     private String dateStarted;
     private String dateFinished;
 
+public Book() {
 
+}
+
+public Book(String title, String author) {
+    this.title = title;
+    this.author = author;
+}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getDateStarted() {
+        return dateStarted;
+    }
+
+    public void setDateStarted(String dateStarted) {
+        this.dateStarted = dateStarted;
+    }
+
+    public String getDateFinished() {
+        return dateFinished;
+    }
+
+    public void setDateFinished(String dateFinished) {
+        this.dateFinished = dateFinished;
+    }
 
 }
