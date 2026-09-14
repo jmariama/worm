1d3484d356dc50f2a7c2bd677fb2f77005c97708
mariama mercy <jmariamercy@gmail.com>
2026-09-14 03:31:51 -0400

    api updates

--- src/main/java/com/github/jmariama/worm/WormApplication.java
+++ src/main/java/com/github/jmariama/worm/WormApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WormApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WormApplication.class, args);
 	}
 
